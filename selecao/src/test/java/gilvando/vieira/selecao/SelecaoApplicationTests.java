67feeae11838680080297c3c1c523078059554bd
Gilvando Lucas Vieira <gilvandolucasvieira@gmail.com>
2020-04-30 11:55:11 -0300

    Bootstrap the project

---
+++ selecao/src/test/java/gilvando/vieira/selecao/SelecaoApplicationTests.java
@@ -0,0 +1,13 @@
+package gilvando.vieira.selecao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelecaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
