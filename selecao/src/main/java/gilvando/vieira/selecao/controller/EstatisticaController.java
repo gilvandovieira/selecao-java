f959bcf98994c7c4ce57c306760823d6b217a155
Gilvando Lucas Vieira <gilvandolucasvieira@gmail.com>
2020-05-02 22:49:47 -0300

    Update EstatisticaController.java

--- selecao/src/main/java/gilvando/vieira/selecao/controller/EstatisticaController.java
+++ selecao/src/main/java/gilvando/vieira/selecao/controller/EstatisticaController.java
@@ -1,5 +1,6 @@
 package gilvando.vieira.selecao.controller;
 
+import gilvando.vieira.selecao.model.MediaPorBandeira;
 import gilvando.vieira.selecao.model.MediaPorMunicipio;
 import gilvando.vieira.selecao.service.EstatisticaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +25,15 @@ public class EstatisticaController {
     }
 
 
-    @GetMapping(path = "/media-municipios")
+    @GetMapping(path = "/media-municipios", produces = "application/json")
     public List<MediaPorMunicipio> getMediaPrecoCombustivelPorMunicipio(){
         return estatisticaService.listaValorMedioPorMunicipio();
 
+    }
+
+    @GetMapping(path = "/media-bandeira", produces = "application/json")
+    public List<MediaPorBandeira> getMediaPrecoPorBandeira(){
+
+        return null;
     }
 }
