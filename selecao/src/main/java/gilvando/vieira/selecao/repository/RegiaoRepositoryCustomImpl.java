e4ee5c602bd1fafac340b2cfa31e73a1820495ee
Gilvando Lucas Vieira <gilvandolucasvieira@gmail.com>
2020-05-02 22:50:05 -0300

    Update RegiaoRepositoryCustomImpl.java

--- selecao/src/main/java/gilvando/vieira/selecao/repository/RegiaoRepositoryCustomImpl.java
+++ selecao/src/main/java/gilvando/vieira/selecao/repository/RegiaoRepositoryCustomImpl.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RegiaoRepositoryCustomImpl implements RegiaoRepositoryCustom {
     @PersistenceContext
@@ -13,11 +14,11 @@ public class RegiaoRepositoryCustomImpl implements RegiaoRepositoryCustom {
 
     @Override
     public List<MediaPorMunicipio> getMediaPorMunicipio() {
-        Query q = entityManager.createNativeQuery("SELECT r.municipio, t.produto, avg(t.valor_Compra) as media_Compra, avg(t.valor_Venda) as media_Venda " +
+        Query q = entityManager.createNativeQuery("SELECT DISTINCT r.municipio, t.produto, avg(t.valor_Compra) as media_Compra, avg(t.valor_Venda) as media_Venda " +
                 "from sel_Regiao r join sel_Revenda re on r.id = re.regiao_id join sel_Transacao t on re.id = t.revenda_id " +
                         "where  t.valor_Compra is not null and t.valor_Venda is not null group by r.municipio, t.produto", MediaPorMunicipio.class);
 
-        List<MediaPorMunicipio> mediaPorMunicipio =  q.getResultList();
+        List<MediaPorMunicipio> mediaPorMunicipio = (List<MediaPorMunicipio>) q.getResultList().stream().distinct().collect(Collectors.toList());
         return mediaPorMunicipio;
     }
 }
